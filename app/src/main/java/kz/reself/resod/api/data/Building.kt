57173b229d7613b6fa8743e5bfabe371a0092757
Yerbolat <twinspazyl@gmail.com>
2022-03-30 11:17:29 +0600

    building logic doing com1

---
+++ app/src/main/java/kz/reself/resod/api/data/Building.kt
@@ -0,0 +1,10 @@
+package kz.reself.resod.api.data
+
+data class Building(
+    val companyName: String,
+    val price: Int,
+    val area: Double,
+    val numberOfRooms: Int,
+    val address: String,
+    val description: String
+)
