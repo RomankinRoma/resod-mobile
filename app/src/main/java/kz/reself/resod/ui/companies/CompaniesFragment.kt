cf426c9d391b7a6b7d48d21c7842aab9429f43b6
Yerbolat <twinspazyl@gmail.com>
2022-04-23 23:23:39 +0600

    add login fragment

--- app/src/main/java/kz/reself/resod/ui/companies/CompaniesFragment.kt
+++ app/src/main/java/kz/reself/resod/ui/companies/CompaniesFragment.kt
@@ -72,25 +72,27 @@ class CompaniesFragment : Fragment(), CompanyAdapter.Listener {
 private fun generateCompanyList(): List<Company> {
     return listOf(
         Company(
-            "Com 1",
-            "Cpm desc 1",
-            "Com sub desc 1",
-                    R.drawable.com11
+            "Bazis",
+            "BAZIS-А - флагман строительной",
+            "Это жилые и административные здания, торговые и промышленные, здания спорта, культуры и образования, сельхозназначения, здания относящиеся к объектам инфраструктуры, а так же автомобильные и железные дороги, дамбы и плотины, автомобильные развязк",
+                    R.drawable.company1
         ), Company(
-            "Com 2",
-            "Cpm desc 2",
-            "Com sub desc 2",
-            R.drawable.home1
+            "RAMS Qazaqstan",
+            "Строительная компания «RAMS Qazaqstan» Алматы. Компания была создана в августе 2004 года.",
+            "В ноябре 2004 для торгового дома «АБДИ» находящегося по адресу Сейфуллина – Райымбека произведена входная группа.",
+            R.drawable.company2
         ), Company(
-            "Com 3",
-            "Cpm desc 3",
-            "Com sub desc 3",
-            R.drawable.home2
+            "Qazaq Stroy",
+            "О КОМПАНИИ Qazaq Strоy – работает на строительном рынке с 2003 года.",
+            "Qazaq Strоy – работает на строительном рынке с 2003 года.\n" +
+                    "\n" +
+                    "За это время компания зарекомендовала себя как ответственного застройщика предлагающего основной массе населения",
+            R.drawable.company3
         ), Company(
-            "Com 4",
-            "Cpm desc 4",
-            "Com sub desc 4",
-            R.drawable.com11
+            "K7 Group",
+            "Строительный холдинг К7 GROUP — лидер рынка",
+            "Мы предлагаем услуги по проектированию частных домов, коттеджей и коммерческих зданий с учетом всех ваших пожеланий,",
+            R.drawable.company4
         ), Company(
             "Com 5",
             "Cpm desc 5",
